129e18a4823345ed7913de739f358e07074c5dff
mih-8 <mirian.calistro.souza@escola.pr.gov.br>
2025-10-21 16:10:19 -0300

    Add click event to toggle card visibility

--- app.js
+++ app.js
@@ -6,10 +6,25 @@ function criarCartao(categoria, pergunta, resposta) {
   
 cartao.innerHTML = `
   <div class="cartao_conteudo">
-  
+                   <h3>${categoria}</h3>
+                   <div class="cartao_conteudo_pergunta">
+                       <p>${pergunta}</p>
+                   </div>
+                   <div class="cartao_conteudo_resposta">
+                       <p>${resposta}</p>
+                   </div>
+                </div>   
 
+ let respostaEstaVisivel = false;
 
+ function viraCartao() {
+   respostaEstaVisivel = !respostaEstaVisivel
+   cartao.classList.toggle('active', respostaEstaVisivel)
+}
+   cartao.addEventListener('click', viraCartao)
 
+   container.appendChild(cartao)
+}
 
 
 
