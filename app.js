6ab941eeba011c17e4567cc7d15abda4501de919
mih-8 <mirian.calistro.souza@escola.pr.gov.br>
2025-10-21 15:47:41 -0300

    Add function to create a card element

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
+function criarCartao(categoria, pergunta, resposta) {
+  let container = document.getElementById('container');
+  let cartao = document.createElement('article');
+  cartao.className = 'cartao';
+
+  
+cartao.innerHTML = `
+  <div class="cartao_conteudo">
+  
+
+
+
+
+
+
+
+
 
