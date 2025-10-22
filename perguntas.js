f9af428f8261289263781879f64a057256433a31
mih-8 <mirian.calistro.souza@escola.pr.gov.br>
2025-10-22 16:26:05 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,1 +1,39 @@
+criarCartao(
+  'Futebol',
+  'Qual seleção tem mais copas do mundo?',
+  'Brasil'
+  )
 
+criarCartao(
+  'Princesas',
+  'Qual princesa tem cabelos longos?',
+  'Rapunzel'
+  )
+
+criarCartao(
+  'Rock',
+  'Quem era conhecido como "O príncipe das trevas"?,
+  'Ozzy Osbourne'
+  )
+
+criarCartao(
+  'Séries',
+  'Qual série tem dois irmãos vampiros com sobrenome Salvatore?',
+  'Diários de um vampiro'
+  )
+
+criarCartao(
+  'Mitologia grega',
+  'Qual Deus grego é conhecido por "Deus da guerra"?',
+  'Ares'
+  )
+
+criarCartao(
+  'Hérois',
+  'Qual herói tem um morcego no peito?',
+  'Batman'
+  )
+
+
+  
+  
